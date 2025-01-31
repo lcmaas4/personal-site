3b0fed15597715c641dadf9f068b882944ed06bc
lcmaas4 <lcmaas4@gmail.com>
2025-01-30 20:00:08 -0500

    navbar and hero content small screen fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,36 +4,65 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Navbar.css';
 
 function Navbar() {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
-      <ul className="navbar-menu">
-        <li className="navbar-item">
-          <a href="#home" className="navbar-link">Home</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#experience" className="navbar-link">Experience</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#projects" className="navbar-link">Projects</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#resume" className="navbar-link">Resume</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#about" className="navbar-link">About</a>
-        </li>
-        <li className="navbar-item">
-          <a href="https://www.linkedin.com/in/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-        </li>
-        <li className="navbar-item">
-          <a href="https://github.com/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    (windowWidth > 600) ?
+      <nav className="navbar">
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <a href="#home" className="navbar-link">Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#experience" className="navbar-link">Experience</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#projects" className="navbar-link">Projects</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#resume" className="navbar-link">Resume</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#about" className="navbar-link">About</a>
+          </li>
+          <li className="navbar-item">
+            <a href="https://www.linkedin.com/in/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="https://github.com/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    : 
+      <nav className="navbar">
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <a href="https://www.linkedin.com/in/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="https://github.com/lcmaas4" className="navbar-link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </nav>
   );
 }
 
